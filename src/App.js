29b143ece959a9c817136b62e74176f7421883e3
sivakumarssk <sivakumarslks@gmail.com>
2024-01-11 09:38:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Localdata from './components/LocalStorage';
+import DashBoard from './mainComponents/DashBoard';
+import ProductsPage from './mainComponents/ProductPage';
+import AccountPage from './mainComponents/AccountPage';
+import Login from './mainComponents/LoginPage';
+import NavBar from './components/NavBar';
+import AddNewProduct from './mainComponents/AddNewProduct';
+import { useAuth } from './components/Auth';
+import PrivateRouter from './components/PrivateRouter';
+import { Navigate } from 'react-router-dom';
 
 function App() {
+  const{user}=useAuth()
+
+
+    const status = window.localStorage.getItem("loginStatus");
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!status && <Localdata/>}
+      <NavBar/>
+      <Routes>
+      <Route
+          path="/"
+          element={status==="true" ? <Navigate to="/dashboard" /> : <Navigate to="/login" />}
+        /> 
+        
+        <Route path='/dashboard' element={<PrivateRouter><DashBoard/></PrivateRouter>} />
+        <Route path='/product' element={<PrivateRouter><ProductsPage/></PrivateRouter>}/>
+        <Route path='/account' element={<PrivateRouter><AccountPage/></PrivateRouter>}/>
+        <Route path='/addProduct' element={<PrivateRouter><AddNewProduct/></PrivateRouter>}/>
+        <Route path='*' element={<PrivateRouter>< DashBoard/></PrivateRouter>} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path="*" element={<PrivateRouter><DashBoard/></PrivateRouter>}/>
+
+      </Routes>
     </div>
   );
 }
